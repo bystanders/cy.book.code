853235e7fe35b5398441f60a7729abce9bbfa7f9
陈宇(ChenYu) <chenyu872891@126.com>
2015-01-28 16:33:38 +0800

    sample.3.6.php done

--- webbots,.spiders,.and.screen.scrapers.2nd/sample.3.6.php
+++ webbots,.spiders,.and.screen.scrapers.2nd/sample.3.6.php
@@ -1,3 +1,19 @@
 <?php
 
+include("LIB_http.php");
+
+$target = "http://www.schrenk.com/publications.php";
+$ref = "http://www.schrenk.com";
+
+$return_array = http_get_withheader($target, $ref);
+
+echo "FILE CONTENTS \n";
+var_dump($return_array['FILE']);
+
+echo "ERRORS \n";
+var_dump($return_array['ERROR']);
+
+echo "STATUS \n";
+var_dump($return_array['STATUS']);
+
 ?>
