f147172c7124cccaf8aa0b3fc061fb3dfba12a80
陈宇(ChenYu) <chenyu872891@126.com>
2015-01-28 16:24:02 +0800

    sample.3.1.php sample.3.2.php done

--- webbots,.spiders,.and.screen.scrapers.2nd/sample.3.2.php
+++ webbots,.spiders,.and.screen.scrapers.2nd/sample.3.2.php
@@ -1,3 +1,11 @@
 <?php
 
+$target = "http://www.WebbotsSpidersScreenScrapers.com/hello_world.html";
+$downloaded_page_array = file($target);
+
+for ($xx=0; $xx < count($downloaded_page_array); $xx++)
+{
+    echo $downloaded_page_array[$xx];    
+}
+
 ?>
