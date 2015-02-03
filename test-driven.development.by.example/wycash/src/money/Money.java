1933d33b4aad7d9916e716dfab3d07514fade568
陈宇(ChenYu) <chenyu872891@126.com>
2015-02-03 17:50:23 +0800

    TDD chapter 5/6/7/8/9/10/11 done

---
+++ test-driven.development.by.example/wycash/src/money/Money.java
@@ -0,0 +1,38 @@
+package money;
+
+public class Money {
+
+	protected int amount;
+	protected String currency;
+
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	public boolean equals(Object object) {
+		Money money = (Money) object;
+		return amount == money.amount && currency().equals(money.currency());
+	}
+
+	public String toString() {
+		return amount + " " + currency;
+	}
+
+	static Money dollar(int amount) {
+		return new Money(amount, "USD");
+	}
+
+	static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
+
+	protected String currency() {
+		return currency;
+	}
+
+	protected Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
+
+}
