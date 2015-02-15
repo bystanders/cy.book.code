65502c2ba6eb07238afc89342d84330dc805e7c4
陈宇(ChenYu) <chenyu872891@126.com>
2015-02-15 17:40:02 +0800

    TDD chapter 21/22/23 done

--- test-driven.development.by.example/pyUnit/pyUnit/pyUnit.py
+++ test-driven.development.by.example/pyUnit/pyUnit/pyUnit.py
@@ -9,11 +9,16 @@ class TestCase:
     def setUp(self):
         pass
 
-    def run(self):
+    def run(self, result):
+        result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
+        return result
 
     def tearDown(self):
         pass
@@ -29,5 +34,36 @@ class WasRun(TestCase):
     def testMethod(self):
         self.log = self.log + " testMethod"
 
+    def testBrokenMethod(self):
+        raise Exception
+
     def tearDown(self):
         self.log = self.log + " tearDown"
+
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
+
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
+
+
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+        return result
--- test-driven.development.by.example/pyUnit/tests/pyUnit_tests.py
+++ test-driven.development.by.example/pyUnit/tests/pyUnit_tests.py
@@ -1,12 +1,45 @@
 # -*- coding: utf-8 -*-
 #from nose.tools import *
-from pyUnit.pyUnit import WasRun, TestCase
+from pyUnit.pyUnit import WasRun, TestCase, TestResult, TestSuite
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
+
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        self.result = suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
+
+    def testFailedResultFormatting(self):
+        self.result.testStarted()
+        self.result.testFailed()
+        assert("1 run, 1 failed" == self.result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        self.result = test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        self.result = test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
+
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        test.run()
+        test.run(self.result)
         assert("setUp testMethod tearDown" == test.log)
 
-TestCaseTest("testTemplateMethod").run()
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testSuite"))
+result = TestResult()
+suite.run(result)
+print result.summary()
